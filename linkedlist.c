f113c44160e26efa94f011ec2fb8230ba88fe0dc
Davide Zanetti <92.davidezanetti@gmail.com>
2018-12-14 20:32:37 +0100

    modifiche a linkedlist V 1.0

--- linkedlist.c
+++ linkedlist.c
@@ -8,6 +8,8 @@
 #define DEL 3
 #define DELETA 4 
 #define DELNOME 5 
+#define ADDTESTA 6 
+#define DELTESTA 5 
 #define LEN 20
 /////////////////////////////////////////////////
 typedef struct persona{   /* crea la struttura */ 
@@ -74,6 +76,25 @@ last->next=NULL;
 free(last);
 }
 /////////////////////////////////////////
+void addTesta(persona **p){
+persona *new = malloc(sizeof(persona));
+printf("Insert N: ");                              
+scanf("%d", &new->eta);                          
+printf("Insert Name: ");                           
+scanf("%s", new->nome);   
+new->next=*p;                          
+*p=new;     
+}
+/////////////////////////////////////////7
+void delTesta(persona **p){
+persona *new = malloc(sizeof(persona));
+new=(*p)->next;
+free(*p);
+*p=new;
+}
+//////////////////////////////////
+
+
 int main(){
 persona *radice = malloc(sizeof(persona)); //creo nodo radice
 printf("Insert radice N: ");
@@ -84,7 +105,7 @@ radice->next=NULL;
 int x=100;
 printf("\n\n");
 while(x!=STOP){                            // commandi
-printf("1 per add\n2 per stampa\n3 per del\n4 cancellare un numero specifivo\n0 per chiudere\n5 per cancellare il nome\n6 aggiungere in testa\n\n");
+printf("1 per add\n2 per stampa\n3 per del\n4 cancellare un numero specifivo\n0 per chiudere\n5 per cancellare il nome\n6 aggiungere in testa\n7 per cancellare testa\n\n");
 scanf("%x",&x);
 printf("\n");
 if(x==ADD){add(radice);printf("\n");}
@@ -92,7 +113,8 @@ if(x==STAMPA){stampa(radice);printf("\n");}
 if(x==DEL){del(radice);printf("\n");}
 if(x==DELETA){delEta(radice);printf("\n");}
 if(x==DELNOME){delNome(radice);printf("\n");}
-
+if(x==ADDTESTA){addTesta(&radice);printf("\n");}
+if(x==DELTESTA){delTesta(&radice);printf("\n");}
 }
 }
 
